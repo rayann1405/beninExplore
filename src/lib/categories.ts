814891accfdbd1c6e0bbbb088626b9894b68e959
rayann1405 <rayannbachabi0@gmail.com>
2026-08-03 15:07:39 +0100

    feat: map data layer, categories and i18n

---
+++ src/lib/categories.ts
@@ -0,0 +1,27 @@
+export const categoryConfig = {
+  nature: {
+    color: '#4CAF7A', // Vert palmier clair
+    hex: 0x4CAF7A,
+    icon: 'Leaf'
+  },
+  culture: {
+    color: '#E8A93A', // Or royal
+    hex: 0xE8A93A,
+    icon: 'Users'
+  },
+  histoire: {
+    color: '#C1440E', // Latérite
+    hex: 0xC1440E,
+    icon: 'BookOpen'
+  },
+  ville: {
+    color: '#8FA6C7', // Bleu ciel
+    hex: 0x8FA6C7,
+    icon: 'Building'
+  },
+  monument: {
+    color: '#E86A34', // Latérite claire
+    hex: 0xE86A34,
+    icon: 'Landmark'
+  }
+};
